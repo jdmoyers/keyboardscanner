a9e4107166fc08e25cadef4c8eb19f23fce95c36
Joshua Moyers <josh@moyers.dev>
2023-01-28 17:16:38 -0500

    Modifying code to work with a 76-key Yamaha PSR-EW310

--- keyboardscanner.ino
+++ keyboardscanner.ino
@@ -1,26 +1,23 @@
 /*
 Moura's Keyboard Scanner: turn you broken (or unused) keyboard in a MIDI controller
 Copyright (C) 2017 Daniel Moura <oxe@oxesoft.com>
-
 This code is originally hosted at https://github.com/oxesoft/keyboardscanner
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <DIO2.h> // install the library DIO2
+#include <MIDIUSB.h>
 
-#define KEYS_NUMBER 61
+#define KEYS_NUMBER 76
 
 #define KEY_OFF               0
 #define KEY_START             1
@@ -41,278 +38,343 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //the following configuration is specific for PSR530
 //thanks Leandro Meucchi, from Argentina, by the PDF
 //take a look at the scheme detailed in PSR530.pdf and modify the following mapping according to the wiring of your keyboard
-#define PIN_A1  33
-#define PIN_A2  32
-#define PIN_A3  31
-#define PIN_A4  30
-#define PIN_A5  29
-#define PIN_A6  28
-#define PIN_A7  27
-#define PIN_A8  26
-#define PIN_A9  25
-#define PIN_A10 24
-#define PIN_A11 23
-#define PIN_A12 22
-#define PIN_B1  40
-#define PIN_B2  39
-#define PIN_B3  38
-#define PIN_B4  37
-#define PIN_B5  36
-#define PIN_B6  35
-#define PIN_B7  34
-#define PIN_C1  45
-#define PIN_C2  44
-#define PIN_C3  43
-#define PIN_C4  42
-#define PIN_C5  41
+#define PIN_A1  22
+#define PIN_A2  24
+#define PIN_A3  28
+#define PIN_A4  26
+#define PIN_A5  32
+#define PIN_A6  30
+#define PIN_A7  36
+#define PIN_A8  34
+#define PIN_A9  40
+#define PIN_A10 38
+#define PIN_A11 44
+#define PIN_A12 42
+
+#define PIN_B1  39
+#define PIN_B2  41
+#define PIN_B3  43
+#define PIN_B4  45
+#define PIN_B5  47
+#define PIN_B6  49
+#define PIN_B7  23
+#define PIN_B8  25
+#define PIN_B9  27
+#define PIN_B10 29
+#define PIN_B11 31
+#define PIN_B12 33
+#define PIN_B13 35
+#define PIN_B14 37
+
 
 byte output_pins[] = {
-    PIN_B6, //C0
-    PIN_B6,
-    PIN_B7,
-    PIN_B7,
-    PIN_B7,
-    PIN_B7,
-    PIN_B7,
-    PIN_B7,
-    PIN_B7,
-    PIN_B7,
-    PIN_B7,
-    PIN_B7,
-    PIN_B7,
-    PIN_B7,
-    PIN_B5,
-    PIN_B5,
-    PIN_B5,
-    PIN_B5,
-    PIN_B5,
-    PIN_B5,
-    PIN_B5,
-    PIN_B5,
-    PIN_B5,
-    PIN_B5,
-    PIN_B5, //C1
-    PIN_B5,
-    PIN_B4,
-    PIN_B4,
-    PIN_B4,
-    PIN_B4,
-    PIN_B4,
-    PIN_B4,
-    PIN_B4,
-    PIN_B4,
-    PIN_B4,
-    PIN_B4,
-    PIN_B4,
-    PIN_B4,
-    PIN_B3,
-    PIN_B3,
-    PIN_B3,
-    PIN_B3,
-    PIN_B3,
-    PIN_B3,
-    PIN_B3,
-    PIN_B3,
-    PIN_B3,
-    PIN_B3,
-    PIN_B3, //C2
-    PIN_B3,
-    PIN_B2,
-    PIN_B2,
-    PIN_B2,
-    PIN_B2,
-    PIN_B2,
-    PIN_B2,
-    PIN_B2,
-    PIN_B2,
-    PIN_B2,
-    PIN_B2,
-    PIN_B2,
-    PIN_B2,
-    PIN_B1,
-    PIN_B1,
-    PIN_B1,
-    PIN_B1,
-    PIN_B1,
-    PIN_B1,
-    PIN_C1,
-    PIN_C1,
-    PIN_C1,
-    PIN_C1,
-    PIN_C1, //C3
-    PIN_C1,
-    PIN_C2,
-    PIN_C2,
-    PIN_C2,
-    PIN_C2,
-    PIN_C2,
-    PIN_C2,
-    PIN_C2,
-    PIN_C2,
-    PIN_C2,
-    PIN_C2,
-    PIN_C2,
-    PIN_C2,
-    PIN_C3,
-    PIN_C3,
-    PIN_C3,
-    PIN_C3,
-    PIN_C3,
-    PIN_C3,
-    PIN_C3,
-    PIN_C3,
-    PIN_C3,
-    PIN_C3,
-    PIN_C3, //C4
-    PIN_C3,
-    PIN_C4,
-    PIN_C4,
-    PIN_C4,
-    PIN_C4,
-    PIN_C4,
-    PIN_C4,
-    PIN_C4,
-    PIN_C4,
-    PIN_C4,
-    PIN_C4,
-    PIN_C4,
-    PIN_C4,
-    PIN_C5,
-    PIN_C5,
-    PIN_C5,
-    PIN_C5,
-    PIN_C5,
-    PIN_C5,
-    PIN_C5,
-    PIN_C5,
-    PIN_C5,
-    PIN_C5,
-    PIN_C5, //C5
-    PIN_C5
+  PIN_B3,
+  PIN_B3,
+  PIN_B3,
+  PIN_B3,
+  PIN_B3,
+  PIN_B3,
+  PIN_B2,
+  PIN_B2,
+  PIN_B2,
+  PIN_B2,
+  PIN_B2,
+  PIN_B2,
+  PIN_B2,
+  PIN_B2,
+  PIN_B2,
+  PIN_B2,
+  PIN_B2,
+  PIN_B2,
+  PIN_B1,
+  PIN_B1,
+  PIN_B1,
+  PIN_B1,
+  PIN_B1,
+  PIN_B1,
+  PIN_B1,
+  PIN_B1,
+  PIN_B1,
+  PIN_B1,
+  PIN_B1,
+  PIN_B1,
+  PIN_B4,
+  PIN_B4,
+  PIN_B4,
+  PIN_B4,
+  PIN_B4,
+  PIN_B4,
+  PIN_B4,
+  PIN_B4,
+  PIN_B4,
+  PIN_B4,
+  PIN_B4,
+  PIN_B4,
+  PIN_B5,
+  PIN_B5,
+  PIN_B5,
+  PIN_B5,
+  PIN_B5,
+  PIN_B5,
+  PIN_B5,
+  PIN_B5,
+  PIN_B5,
+  PIN_B5,
+  PIN_B5,
+  PIN_B5,
+  PIN_B6,
+  PIN_B6,
+  PIN_B6,
+  PIN_B6,
+  PIN_B6,
+  PIN_B6,
+  PIN_B6,
+  PIN_B6,
+  PIN_B6,
+  PIN_B6,
+  PIN_B6,
+  PIN_B6,
+  PIN_B7,
+  PIN_B7,
+  PIN_B7,
+  PIN_B7,
+  PIN_B7,
+  PIN_B7,
+  PIN_B7,
+  PIN_B7,
+  PIN_B7,
+  PIN_B7,
+  PIN_B7,
+  PIN_B7,
+  PIN_B8,
+  PIN_B8,
+  PIN_B8,
+  PIN_B8,
+  PIN_B8,
+  PIN_B8,
+  PIN_B8,
+  PIN_B8,
+  PIN_B8,
+  PIN_B8,
+  PIN_B8,
+  PIN_B8,
+  PIN_B9,
+  PIN_B9,
+  PIN_B9,
+  PIN_B9,
+  PIN_B9,
+  PIN_B9,
+  PIN_B9,
+  PIN_B9,
+  PIN_B9,
+  PIN_B9,
+  PIN_B9,
+  PIN_B9,
+  PIN_B14,
+  PIN_B14,
+  PIN_B14,
+  PIN_B14,
+  PIN_B14,
+  PIN_B14,
+  PIN_B14,
+  PIN_B14,
+  PIN_B14,
+  PIN_B14,
+  PIN_B14,
+  PIN_B14,
+  PIN_B13,
+  PIN_B13,
+  PIN_B13,
+  PIN_B13,
+  PIN_B13,
+  PIN_B13,
+  PIN_B13,
+  PIN_B13,
+  PIN_B13,
+  PIN_B13,
+  PIN_B13,
+  PIN_B13,
+  PIN_B12,
+  PIN_B12,
+  PIN_B12,
+  PIN_B12,
+  PIN_B12,
+  PIN_B12,
+  PIN_B12,
+  PIN_B12,
+  PIN_B12,
+  PIN_B12,
+  PIN_B12,
+  PIN_B12,
+  PIN_B11,
+  PIN_B11,
+  PIN_B11,
+  PIN_B11,
+  PIN_B11,
+  PIN_B11,
+  PIN_B11,
+  PIN_B11,
+  PIN_B11,
+  PIN_B11,
+  PIN_B11,
+  PIN_B11,
+  PIN_B10,
+  PIN_B10,
 };
+
 byte input_pins[] = {
-    PIN_A9, //C0
-    PIN_A10,
-    PIN_A9,
-    PIN_A10,
-    PIN_A6,
-    PIN_A5,
-    PIN_A8,
-    PIN_A7,
-    PIN_A3,
-    PIN_A4,
-    PIN_A1,
-    PIN_A2,
-    PIN_A11,
-    PIN_A12,
-    PIN_A11,
-    PIN_A12,
-    PIN_A1,
-    PIN_A2,
-    PIN_A3,
-    PIN_A4,
-    PIN_A8,
-    PIN_A7,
-    PIN_A6,
-    PIN_A5,
-    PIN_A9, //C1
-    PIN_A10,
-    PIN_A9,
-    PIN_A10,
-    PIN_A6,
-    PIN_A5,
-    PIN_A8,
-    PIN_A7,
-    PIN_A3,
-    PIN_A4,
-    PIN_A1,
-    PIN_A2,
-    PIN_A11,
-    PIN_A12,
-    PIN_A11,
-    PIN_A12,
-    PIN_A1,
-    PIN_A2,
-    PIN_A3,
-    PIN_A4,
-    PIN_A8,
-    PIN_A7,
-    PIN_A6,
-    PIN_A5,
-    PIN_A9, //C2
-    PIN_A10,
-    PIN_A9,
-    PIN_A10,
-    PIN_A1,
-    PIN_A2,
-    PIN_A6,
-    PIN_A5,
-    PIN_A8,
-    PIN_A7,
-    PIN_A3,
-    PIN_A4,
-    PIN_A11,
-    PIN_A12,
-    PIN_A11,
-    PIN_A12,
-    PIN_A3,
-    PIN_A4,
-    PIN_A9,
-    PIN_A10,
-    PIN_A6,
-    PIN_A5,
-    PIN_A8,
-    PIN_A7,
-    PIN_A1, //C3
-    PIN_A2,
-    PIN_A1,
-    PIN_A2,
-    PIN_A6,
-    PIN_A5,
-    PIN_A8,
-    PIN_A7,
-    PIN_A3,
-    PIN_A4,
-    PIN_A9,
-    PIN_A10,
-    PIN_A11,
-    PIN_A12,
-    PIN_A11,
-    PIN_A12,
-    PIN_A1,
-    PIN_A2,
-    PIN_A3,
-    PIN_A4,
-    PIN_A8,
-    PIN_A7,
-    PIN_A6,
-    PIN_A5,
-    PIN_A9, //C4
-    PIN_A10,
-    PIN_A9,
-    PIN_A10,
-    PIN_A11,
-    PIN_A12,
-    PIN_A6,
-    PIN_A5,
-    PIN_A8,
-    PIN_A7,
-    PIN_A3,
-    PIN_A4,
-    PIN_A1,
-    PIN_A2,
-    PIN_A1,
-    PIN_A2,
-    PIN_A11,
-    PIN_A12,
-    PIN_A3,
-    PIN_A4,
-    PIN_A8,
-    PIN_A7,
-    PIN_A6,
-    PIN_A5,
-    PIN_A9, //C5
-    PIN_A10
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12, 
+  PIN_A11,
+  PIN_A9, 
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
+  PIN_A4,
+  PIN_A3,
+  PIN_A2,
+  PIN_A1,
+  PIN_A12,
+  PIN_A11,
+  PIN_A9,
+  PIN_A10,
+  PIN_A8,
+  PIN_A7,
+  PIN_A5,
+  PIN_A6,
 };
 
 //cheap keyboards often has the black keys softer or harder than the white ones
@@ -320,15 +382,16 @@ byte input_pins[] = {
 //#define BLACK_KEYS_CORRECTION
 
 #ifdef BLACK_KEYS_CORRECTION
-#define MULTIPLIER 192 // 127 is the central value (corresponding to 1.0)
-byte black_keys[] = {
+  #define MULTIPLIER 192 // 127 is the central value (corresponding to 1.0)
+
+  byte black_keys[] = {
     0,1,0,1,0,0,1,0,1,0,1,0,
     0,1,0,1,0,0,1,0,1,0,1,0,
     0,1,0,1,0,0,1,0,1,0,1,0,
     0,1,0,1,0,0,1,0,1,0,1,0,
     0,1,0,1,0,0,1,0,1,0,1,0,
     0
-};
+  };
 #endif
 
 //uncomment the next line to inspect the number of scans per seconds
@@ -348,162 +411,207 @@ boolean       signals[KEYS_NUMBER * 2];
 boolean       pedal_enabled;
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(13, OUTPUT);
-    digitalWrite(13, LOW);
-    int i;
-    for (i = 0; i < KEYS_NUMBER; i++)
-    {
-        keys_state[i] = KEY_OFF;
-        keys_time[i] = 0;
-    }
-    for (byte pin = 0; pin < sizeof(output_pins); pin++)
-    {
-        pinMode(output_pins[pin], OUTPUT);
-    }
-    for (byte pin = 0; pin < sizeof(input_pins); pin++)
-    {
-        pinMode(input_pins[pin], INPUT_PULLUP);
-    }
-    pinMode(PEDAL_PIN, INPUT_PULLUP);
-    pedal_enabled = digitalRead(PEDAL_PIN) != HIGH;
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+
+  int i;
+  
+  for (i = 0; i < KEYS_NUMBER; i++)
+  {
+    keys_state[i] = KEY_OFF;
+    keys_time[i] = 0;
+  }
+  
+  for (byte pin = 0; pin < sizeof(output_pins); pin++)
+  {
+    pinMode(output_pins[pin], OUTPUT);
+  }
+
+  for (byte pin = 0; pin < sizeof(input_pins); pin++)
+  {
+    pinMode(input_pins[pin], INPUT_PULLUP);
+  }
+
+  pinMode(PEDAL_PIN, INPUT_PULLUP);
+  pedal_enabled = digitalRead(PEDAL_PIN) != HIGH;
+}
+
+void noteOn(byte channel, byte pitch, byte velocity)
+{
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity)
+{
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
 }
 
 void send_midi_event(byte status_byte, byte key_index, unsigned long time)
 {
-    unsigned long t = time;
-#ifdef BLACK_KEYS_CORRECTION
+  unsigned long t = time;
+
+  #ifdef BLACK_KEYS_CORRECTION
     if (black_keys[key_index])
     {
-        t = (t * MULTIPLIER) >> 7;
+      t = (t * MULTIPLIER) >> 7;
     }
-#endif
-    if (t > MAX_TIME_MS)
-        t = MAX_TIME_MS;
-    if (t < MIN_TIME_MS)
-        t = MIN_TIME_MS;
-    t -= MIN_TIME_MS;
-    unsigned long velocity = 127 - (t * 127 / MAX_TIME_MS_N);
-    byte vel = (((velocity * velocity) >> 7) * velocity) >> 7;
-    byte key = 36 + key_index;
-#ifdef DEBUG_MIDI_MESSAGE
+  #endif
+
+  if (t > MAX_TIME_MS)
+      t = MAX_TIME_MS;
+  if (t < MIN_TIME_MS)
+      t = MIN_TIME_MS;
+
+  t -= MIN_TIME_MS;
+
+  unsigned long velocity = 127 - (t * 127 / MAX_TIME_MS_N);
+  byte vel = (((velocity * velocity) >> 7) * velocity) >> 7;
+  byte key = 16 + key_index;
+  #ifdef DEBUG_MIDI_MESSAGE
     char out[32];
     sprintf(out, "%02X %02X %03d %d", status_byte, key, vel, time);
     Serial.println(out);
-#else
-    Serial.write(status_byte);
-    Serial.write(key);
-    Serial.write(vel);
-#endif
+  #else
+    // Serial.write(status_byte);
+    // Serial.write(key);
+    // Serial.write(vel);
+
+    if(status_byte == 0x80) {
+      noteOff(0, key, vel);
+    } else {
+      Serial.println(key);
+      noteOn(0, key, vel);
+    }
+  #endif
 }
 
 void loop() {
-#ifdef DEBUG_SCANS_PER_SECOND
+  #ifdef DEBUG_SCANS_PER_SECOND
     static unsigned long cycles = 0;
     static unsigned long start = 0;
     static unsigned long current = 0;
+
     cycles++;
     current = millis();
+
     if (current - start >= 1000)
     {
-        Serial.println(cycles);
-        cycles = 0;
-        start = current;
+      Serial.println(cycles);
+      cycles = 0;
+      start = current;
     }
-#endif
-    byte pedal = LOW;
-    if (pedal_enabled)
+  #endif
+  byte pedal = LOW;
+
+  if (pedal_enabled)
+  {
+    pedal = digitalRead2(PEDAL_PIN);
+  }
+  
+  boolean *s = signals;
+
+  for (byte i = 0; i < KEYS_NUMBER * 2; i++)
+  {
+    byte output_pin = output_pins[i];
+    byte input_pin = input_pins[i];
+    digitalWrite2(output_pin, LOW);
+    *(s++) = !digitalRead2(input_pin);
+    digitalWrite2(output_pin, HIGH);
+  }
+
+  byte          *state  = keys_state;
+  unsigned long *ktime  = keys_time;
+  boolean       *signal = signals;
+
+  for (byte key = 0; key < KEYS_NUMBER; key++)
+  {
+    for (byte state_index = 0; state_index < 2; state_index++)
     {
-        pedal = digitalRead2(PEDAL_PIN);
-    }
-   
-    boolean *s = signals;
-    for (byte i = 0; i < KEYS_NUMBER * 2; i++)
-    {
-        byte output_pin = output_pins[i];
-        byte input_pin = input_pins[i];
-        digitalWrite2(output_pin, LOW);
-        *(s++) = !digitalRead2(input_pin);
-        digitalWrite2(output_pin, HIGH);
+      switch (*state)
+      {
+        case KEY_OFF:
+          if (state_index == 0 && *signal)
+          {
+            *state = KEY_START;
+            *ktime = millis();
+          }
+
+          break;
+
+        case KEY_START:
+          if (state_index == 0 && !*signal)
+          {
+            *state = KEY_OFF;
+            break;
+          }
+          
+          if (state_index == 1 && *signal)
+          {
+            *state = KEY_ON;
+            send_midi_event(0x90, key, millis() - *ktime);
+          }
+          break;
+
+        case KEY_ON:
+          if (state_index == 1 && !*signal)
+          {
+            *state = KEY_RELEASED;
+            *ktime = millis();
+          }
+          break;
+
+        case KEY_RELEASED:
+          if (state_index == 0 && !*signal)
+          {
+            if (pedal)
+            {
+              *state = KEY_SUSTAINED;
+              break;
+            }
+
+            *state = KEY_OFF;
+            send_midi_event(0x80, key, millis() - *ktime);
+          }
+          break;
+
+        case KEY_SUSTAINED:
+          if (!pedal)
+          {
+            *state = KEY_OFF;
+            send_midi_event(0x80, key, MAX_TIME_MS);
+          }
+
+          if (state_index == 0 && *signal)
+          {
+            *state = KEY_SUSTAINED_RESTART;
+            *ktime = millis();
+          }
+          break;
+          
+        case KEY_SUSTAINED_RESTART:
+          if (state_index == 0 && !*signal)
+          {
+            *state = KEY_SUSTAINED;
+            digitalWrite(13, HIGH);
+            break;
+          }
+
+          if (state_index == 1 && *signal)
+          {
+            *state = KEY_ON;
+            send_midi_event(0x80, key, MAX_TIME_MS);
+            send_midi_event(0x90, key, millis() - *ktime);
+          }
+          break;
+      }
+
+      signal++;
     }
 
-    byte          *state  = keys_state;
-    unsigned long *ktime  = keys_time;
-    boolean       *signal = signals;
-    for (byte key = 0; key < KEYS_NUMBER; key++)
-    {
-        for (byte state_index = 0; state_index < 2; state_index++)
-        {
-            switch (*state)
-            {
-            case KEY_OFF:
-                if (state_index == 0 && *signal)
-                {
-                    *state = KEY_START;
-                    *ktime = millis();
-                }
-                break;
-            case KEY_START:
-                if (state_index == 0 && !*signal)
-                {
-                    *state = KEY_OFF;
-                    break;
-                }
-                if (state_index == 1 && *signal)
-                {
-                    *state = KEY_ON;
-                    send_midi_event(0x90, key, millis() - *ktime);
-                }
-                break;
-            case KEY_ON:
-                if (state_index == 1 && !*signal)
-                {
-                    *state = KEY_RELEASED;
-                    *ktime = millis();
-                }
-                break;
-            case KEY_RELEASED:
-                if (state_index == 0 && !*signal)
-                {
-                    if (pedal)
-                    {
-                        *state = KEY_SUSTAINED;
-                        break;
-                    }
-                    *state = KEY_OFF;
-                    send_midi_event(0x80, key, millis() - *ktime);
-                }
-                break;
-            case KEY_SUSTAINED:
-                if (!pedal)
-                {
-                    *state = KEY_OFF;
-                    send_midi_event(0x80, key, MAX_TIME_MS);
-                }
-                if (state_index == 0 && *signal)
-                {
-                    *state = KEY_SUSTAINED_RESTART;
-                    *ktime = millis();
-                }
-                break;
-            case KEY_SUSTAINED_RESTART:
-                if (state_index == 0 && !*signal)
-                {
-                    *state = KEY_SUSTAINED;
-                    digitalWrite(13, HIGH);
-                    break;
-                }
-                if (state_index == 1 && *signal)
-                {
-                    *state = KEY_ON;
-                    send_midi_event(0x80, key, MAX_TIME_MS);
-                    send_midi_event(0x90, key, millis() - *ktime);
-                }
-                break;
-            }
-            signal++;
-        }
-        state++;
-        ktime++;
-    }
+    state++;
+    ktime++;
+  }
 }
